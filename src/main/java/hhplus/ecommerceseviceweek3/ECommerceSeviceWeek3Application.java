8174f1826f82cd6c113a31a90195358fef6948e2
sibyun <si.byun@ecoinsight.co.kr>
2024-04-08 17:29:25 +0900

    최초 커밋

---
+++ src/main/java/hhplus/ecommerceseviceweek3/ECommerceSeviceWeek3Application.java
@@ -0,0 +1,13 @@
+package hhplus.ecommerceseviceweek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSeviceWeek3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceSeviceWeek3Application.class, args);
+    }
+
+}
---
+++ src/main/java/hhplus/ecommerceseviceweek3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package hhplus.ecommerceseviceweek3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ECommerceSeviceWeek3Application.class);
+    }
+
+}
